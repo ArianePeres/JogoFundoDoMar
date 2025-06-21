b7c8f63af25cc97418c0a1c1449aa6a59d6d19ae
ArianeP <ariane.prs@gmail.com>
2025-06-20 23:47:03 -0300

    Changing menu

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -13,17 +13,17 @@ class EntityFactory:
     @staticmethod
     def get_entity(entity_name: str):
         match entity_name:
-            case 'fase':
+            case 'Level1Bg':
                 list_bg = []
                 for i in range(5):  # level1bg images number
-                    list_bg.append(Background(f'fase{i}', (0, 0)))
-                    list_bg.append(Background(f'fase{i}', (WIN_WIDTH, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
-            case 'fundo':
+            case 'Level2Bg':
                 list_bg = []
                 for i in range(5):  # level2bg images number
-                    list_bg.append(Background(f'fundo{i}', (0, 0)))
-                    list_bg.append(Background(f'fundo{i}', (WIN_WIDTH, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
